62cbb371340cecd117acd5dbfec9037c98f58fd1
Duarte Dias <DuartexDias@gmail.com>
2019-08-22 20:23:27 +0100

    trying to get servo current angle

--- backend/routes/servo/rotateRight.js
+++ backend/routes/servo/rotateRight.js
@@ -3,9 +3,11 @@ const servo = new Gpio(10, {mode: Gpio.OUTPUT})
 
 module.exports = app => {
     app.get('/servo-right', function(req, res){
+        console.log('Servo position: ' + servo.getServoPulseWidth())
         console.log('RIGHT -> servo on GPIO10')
-        console.log('Servo input: ' + req.params.servoValue)
         servo.servoWrite(800)
+        console.log('Servo position: ' + servo.getServoPulseWidth())
+        console.log('\n')
         res.send('Set servo angle to 800')
     })
 }

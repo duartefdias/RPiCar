ebba6e89ef718f2d825af594b99de56362d951d6
Duarte Dias <DuartexDias@gmail.com>
2019-08-22 19:46:15 +0100

    changed to pigpio js library for servo controlling

--- backend/routes/servo/test.js
+++ backend/routes/servo/test.js
@@ -1,17 +1,9 @@
-const Gpio = require('onoff').Gpio
-const servo = new Gpio(14, 'out')
+const Gpio = require('pigpio').Gpio
+const servo = new Gpio(10, {mode: Gpio.OUTPUT})
 
 module.exports = app => {
     app.get('/servoTest', function(req, res){
         console.log('Toogling signal of servo on GPIO14')
-        if (servo.readSync() === 0) {
-            console.log('Signal HIGH at GPIO14')
-            servo.writeSync(1); //set output to 1 i.e turn servo on
-          } else {
-            console.log('Signal LOW at GPIO14')
-            servo.writeSync(0); //set output to 0 i.e. turn servo off 
-         }
-         console.log('Exiting servo toogle route')
-         console.log('\n')
+        servo.servoWrite(2000)
     })
 }

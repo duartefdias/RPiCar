3ff076283f269177eef1ca6dd6b72659707b08c8
Duarte Dias <DuartexDias@gmail.com>
2019-08-22 19:48:14 +0100

    debug

--- backend/routes/servo/test.js
+++ backend/routes/servo/test.js
@@ -1,9 +1,9 @@
 const Gpio = require('pigpio').Gpio
-const servo = new Gpio(10, {mode: Gpio.OUTPUT})
+const servo = new Gpio(14, {mode: Gpio.OUTPUT})
 
 module.exports = app => {
     app.get('/servoTest', function(req, res){
         console.log('Toogling signal of servo on GPIO14')
-        servo.servoWrite(2000)
+        servo.servoWrite(1900)
     })
 }

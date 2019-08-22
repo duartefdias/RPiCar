2be1376c4fc81983371c9d1cd8b7f8380c692339
Duarte Dias <DuartexDias@gmail.com>
2019-08-22 22:45:12 +0100

    added servo 2 to backend

---
+++ backend/routes/servo/setServo2Angle.js
@@ -0,0 +1,10 @@
+const Gpio = require('pigpio').Gpio
+const servo = new Gpio(9, {mode: Gpio.OUTPUT})
+
+module.exports = app => {
+    app.get('/servo2Angle/:servoAngle', function(req, res){
+        console.log('Setting servo pwm to: ' + req.params.servoAngle)
+        servo.servoWrite(req.params.servoAngle)
+        res.send(req.params.servoAngle)
+    })
+}

ec75cc29de7aaaf1c87f46bdad3e94f6e7d781f4
Duarte Dias <DuartexDias@gmail.com>
2019-08-22 20:07:28 +0100

    sending servo value in request url

--- backend/routes/servo/test.js
+++ backend/routes/servo/test.js
@@ -2,9 +2,10 @@ const Gpio = require('pigpio').Gpio
 const servo = new Gpio(10, {mode: Gpio.OUTPUT})
 
 module.exports = app => {
-    app.get('/servoTest', function(req, res){
-        console.log('Toogling signal of servo on GPIO14')
-        servo.servoWrite(1900)
-        res.send('Done')
+    app.get('/servoTest/:servoValue', function(req, res){
+        console.log('Toogling signal of servo on GPIO10')
+        console.log('Servo input: ' + req.params.servoValue)
+        servo.servoWrite(req.params.servoValue)
+        res.send('Sent servo value: ' + req.params.servoValue)
     })
 }
